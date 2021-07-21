21863ff7d267f411c4a900f8065206ac9a795129
Wumkin <87764670+tombenten@users.noreply.github.com>
2021-07-21 14:00:59 +0000

    s

--- bot.js
+++ bot.js
@@ -2,7 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 const prefix = "!"
 const fs = require("fs");
-const config = require("./config.json")
+//Bot.js File:
+const keepAlive = require('./server.js');
+const mySecret = process.env['DISCORD_BOT_TOKEN']
+
 
 client.commands = new Discord.Collection();
 
@@ -40,4 +43,6 @@ client.on('message', message => {
 
 
 
-client.login(config.token);
+
+keepAlive();
+client.login(mySecret);
